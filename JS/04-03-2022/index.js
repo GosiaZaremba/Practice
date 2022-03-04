c95701b75fbe696984a8d1784920184013587877
Gosia Zaremba <gosia@szel.net>
2022-03-04 14:47:05 +0100

    JS practice day2

---
+++ JS/04-03-2022/index.js
@@ -0,0 +1,70 @@
+
+// zadanie 1
+//---------------
+
+// function fn() {
+//     console.log('Wykonana!');
+// };
+
+// function invoke(param) {
+//     param();
+// };
+
+// invoke(fn);
+
+// Zadanie 2
+//-------------------------------------
+
+// function fn() {
+//     console.log('Wyokonana!')
+// }
+
+// function invoke(param1,) {
+//     param1()
+// }
+
+// function invMultTim(param1, param2) {
+//     for (let i = 0; i < param2; i++) {
+//         param1()
+//     }
+// }
+// invMultTim(fn, 5);
+
+// Zadanie 3
+// --------------------
+
+// function greeter() {
+//     function innerGreeter() {
+//         console.log('Hello!')
+//     }
+//     return innerGreeter
+// };
+
+// const myGreet = greeter();
+// myGreet();
+
+// Zadanie 4 - uzupełnić!!!!!!!!
+// -------------------------
+
+
+
+// function CustomGreeter(param) {
+//     function innerGreeter() {
+//         console.log('Hello!')
+//     }
+//     return innerGreeter
+// };
+
+
+// const myGreeter = CustomGreeter(innerGreeter, 'Zofia');
+
+// Zadanie 5
+// ----------------------
+
+
+
+
+
+
+
+
---
+++ JS/04-03-2022/index2.js
@@ -0,0 +1,18 @@
+// Zadanie 6
+// --------------
+// const tab = ['el1', 'el2', 'el3', 'el4'];
+
+// if (tab.length > 3) {
+//     console.log('Jest duża')
+// } else {
+//     console.log('Jest mała')
+// }
+// CZEMU TO NIE DZIAŁA ???
+// const tab = ['el1', 'el2', 'el3', 'el4'];
+// const check = (array) => {
+//     if (array.length > 3) {
+//         return 'Jest duża';
+//     } else {
+//         return 'Jest mała';
+//     }
+// };
