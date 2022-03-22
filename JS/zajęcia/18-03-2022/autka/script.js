a866ff83a4ded585bb4358cc932c0522009f597b
Gosia Zaremba <gosia@szel.net>
2022-03-22 23:20:34 +0100

    new version

--- JS/zajęcia/18-03-2022/autka/script.js
+++ JS/zajęcia/18-03-2022/autka/script.js
@@ -3,75 +3,58 @@
 let cars = document.querySelectorAll('.car');
 let red = document.querySelector('.red');
 let blue = document.querySelector('.blue');
-let baton = document.querySelector('button');
+let baton = document.querySelector('.attach');
 let baton2 = document.querySelector('.detach')
-let moveBy = 5;
 
-const start = () => {
+let moveBy = 5;
+let isAttached = false;
+
+const setStartPosition = () => {
     red.style.bottom = '190px';
     red.style.left = '220px';
     blue.style.bottom = '100px';
     blue.style.left = '220px'
 }
 
+const onAttachButton = () => {
+    setStartPosition();
+    isAttached = true;
+};
 
-const nonBtn = event => {
+const onReset = () => {
+    setStartPosition();
+    isAttached = false;
+};
+
+const processKey = event => {
 
     if (event.key == 'ArrowUp') {
         red.style.bottom = parseInt(red.style.bottom) + moveBy + 'px';
-
+        if (isAttached) {
+            blue.style.bottom = parseInt(blue.style.bottom) + moveBy + 'px';
+        }
     }
     else if (event.key == 'ArrowDown') {
         red.style.bottom = parseInt(red.style.bottom) - moveBy + 'px';
-
+        if (isAttached) {
+            blue.style.bottom = parseInt(blue.style.bottom) - moveBy + 'px';
+        }
     }
     else if (event.key == 'ArrowLeft') {
         red.style.left = parseInt(red.style.left) - moveBy + 'px';
-
+        if (isAttached) {
+            blue.style.left = parseInt(blue.style.left) - moveBy + 'px';
+        }
     }
     else if (event.key == 'ArrowRight') {
         red.style.left = parseInt(red.style.left) + moveBy + 'px';
+        if (isAttached) {
+            blue.style.left = parseInt(blue.style.left) + moveBy + 'px';
+        }
     }
 };
 
-
-
-const withBtn = () => {
-    red.style.bottom = '190px';
-    red.style.left = '220px';
-
-    window.removeEventListener('keydown', nonBtn);
-
-    window.addEventListener('keydown', event => {
-
-        if (event.key == 'ArrowUp') {
-            red.style.bottom = parseInt(red.style.bottom) + moveBy + 'px';
-            blue.style.bottom = parseInt(blue.style.bottom) + moveBy + 'px';
-        }
-        else if (event.key == 'ArrowDown') {
-            red.style.bottom = parseInt(red.style.bottom) - moveBy + 'px';
-            blue.style.bottom = parseInt(blue.style.bottom) - moveBy + 'px';
-        }
-        else if (event.key == 'ArrowLeft') {
-            red.style.left = parseInt(red.style.left) - moveBy + 'px';
-            blue.style.left = parseInt(blue.style.left) - moveBy + 'px';
-        }
-        else if (event.key == 'ArrowRight') {
-            red.style.left = parseInt(red.style.left) + moveBy + 'px';
-            blue.style.left = parseInt(blue.style.left) + moveBy + 'px';
-        }
-    })
-}
-
-
-const reset = () => {
-    start();
-    baton.removeEventListener('click', withBtn);
-};
-
-
-
-window.addEventListener('load', start)
-window.addEventListener('keydown', nonBtn);
-baton.addEventListener('click', withBtn);
-baton2.addEventListener('click', reset);
+window.addEventListener('keydown', processKey);
+baton.addEventListener('click', onAttachButton);
+baton2.addEventListener('click', onReset);
+window.addEventListener('load', setStartPosition);
--- JS/zajęcia/18-03-2022/autka/script2.js
+++ JS/zajęcia/18-03-2022/autka/script2.js
@@ -1,14 +1,77 @@
-console.log('hello2')
+// console.log("hello")
 
-let button = document.querySelector('button');
-let street = document.querySelector('.street');
+let cars = document.querySelectorAll('.car');
+let red = document.querySelector('.red');
+let blue = document.querySelector('.blue');
+let baton = document.querySelector('.attach');
+let baton2 = document.querySelector('.detach')
+let moveBy = 5;
 
-
-button.addEventListener('click', function btn(event) {
-    let nowyDivek = document.createElement('div');
-    nowyDivek.classList.add('bigDiv');
-    console.log(nowyDivek);
-    street.append(nowyDivek);
-
+const start = () => {
+    red.style.bottom = '190px';
+    red.style.left = '220px';
+    blue.style.bottom = '100px';
+    blue.style.left = '220px'
 }
-)
+
+
+const nonBtn = event => {
+
+    if (event.key == 'ArrowUp') {
+        red.style.bottom = parseInt(red.style.bottom) + moveBy + 'px';
+
+    }
+    else if (event.key == 'ArrowDown') {
+        red.style.bottom = parseInt(red.style.bottom) - moveBy + 'px';
+
+    }
+    else if (event.key == 'ArrowLeft') {
+        red.style.left = parseInt(red.style.left) - moveBy + 'px';
+
+    }
+    else if (event.key == 'ArrowRight') {
+        red.style.left = parseInt(red.style.left) + moveBy + 'px';
+    }
+};
+
+
+
+const withBtn = () => {
+    red.style.bottom = '190px';
+    red.style.left = '220px';
+
+    window.removeEventListener('keydown', nonBtn);
+
+    window.addEventListener('keydown', event => {
+
+        if (event.key == 'ArrowUp') {
+            red.style.bottom = parseInt(red.style.bottom) + moveBy + 'px';
+            blue.style.bottom = parseInt(blue.style.bottom) + moveBy + 'px';
+        }
+        else if (event.key == 'ArrowDown') {
+            red.style.bottom = parseInt(red.style.bottom) - moveBy + 'px';
+            blue.style.bottom = parseInt(blue.style.bottom) - moveBy + 'px';
+        }
+        else if (event.key == 'ArrowLeft') {
+            red.style.left = parseInt(red.style.left) - moveBy + 'px';
+            blue.style.left = parseInt(blue.style.left) - moveBy + 'px';
+        }
+        else if (event.key == 'ArrowRight') {
+            red.style.left = parseInt(red.style.left) + moveBy + 'px';
+            blue.style.left = parseInt(blue.style.left) + moveBy + 'px';
+        }
+    })
+}
+
+
+const reset = () => {
+    start();
+    baton.removeEventListener('click', withBtn);
+};
+
+
+
+window.addEventListener('load', start)
+window.addEventListener('keydown', nonBtn);
+baton.addEventListener('click', withBtn);
+baton2.addEventListener('click', reset);
