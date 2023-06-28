876d0388b52361b0d7b775f6dd9bfb61485ca00e
Gosia Zaremba <gosia@szel.net>
2023-06-28 16:17:55 +0200

    Fixed css font bug in Table, created and styled Form.

---
+++ HTML_CSS/Form/script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById("form1");
+
+function onSubmit() {
+  console.log(form[1]);
+}
+
+// const firstName = document.getElementsByName("first-name")[0];
+// const lastName = document.getElementsByName("last-name")[0];
+// const email = document.getElementsByName("email")[0];
+// const age = document.getElementsByName("age")[0];
+// const marketingConsent = document.getElementsByClassName(
+//   "checkbox-marketing"[0]
+// );
+// const submitButton = document.getElementsByName("submit-button")[0];
