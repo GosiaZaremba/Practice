1e2025446a59ef7b3d7ad03565656626e28aa0be
Gosia Zaremba <gosia@szel.net>
2023-06-28 14:45:57 +0200

    Table created and styled.

---
+++ HTML_CSS/Table/script.js
@@ -0,0 +1,26 @@
+//Headers
+const thead1 = document.getElementsByTagName("td")[0];
+const thead2 = document.getElementsByTagName("td")[1];
+const thead3 = document.getElementsByTagName("td")[2];
+
+console.log("headers", thead1, thead2, thead3);
+
+//Names
+const name1 = document.getElementsByTagName("td")[3];
+const name2 = document.getElementsByTagName("td")[6];
+const name3 = document.getElementsByTagName("td")[9];
+
+console.log("first names", name1, name2, name3);
+
+//Last names
+const lastName1 = document.getElementsByTagName("td")[4];
+const lastName2 = document.getElementsByTagName("td")[7];
+const lastName3 = document.getElementsByTagName("td")[10];
+
+console.log("last names", lastName1, lastName2, lastName3);
+
+//Results
+const resultCell1 = document.getElementsByTagName("td")[5];
+const resultCell2 = document.getElementsByTagName("td")[8];
+const resultCell3 = document.getElementsByTagName("td")[11];
+console.log("results", resultCell1, resultCell2, resultCell3);
