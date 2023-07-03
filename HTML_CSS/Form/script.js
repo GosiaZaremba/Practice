d36caf48e82610a937637fa1c21483dd43ba6eb9
Gosia Zaremba <gosia@szel.net>
2023-07-03 17:28:01 +0200

    Added validation for all inputs.

--- HTML_CSS/Form/script.js
+++ HTML_CSS/Form/script.js
@@ -1,14 +1,52 @@
-const form = document.getElementById("form1");
+const form = document.forms[0];
+const formInputs = Array.from(form.elements);
 
-function onSubmit() {
-  console.log(form[1]);
+const txtRegEx = /[A-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]/gimu;
+const phoneRegEx = /[0-9]{3}-[0-9]{3}-[0-9]{3}/;
+const emailRegEx =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+function validateText(element) {
+  element.type === "text" && element.value.match(txtRegEx)
+    ? console.log("txt ok")
+    : console.log("Wypełnij poprawnie pola tekstowe");
 }
 
-// const firstName = document.getElementsByName("first-name")[0];
-// const lastName = document.getElementsByName("last-name")[0];
-// const email = document.getElementsByName("email")[0];
-// const age = document.getElementsByName("age")[0];
-// const marketingConsent = document.getElementsByClassName(
-//   "checkbox-marketing"[0]
-// );
-// const submitButton = document.getElementsByName("submit-button")[0];
+function validateNumber(element) {
+  element.type === "tel" && element.value.match(phoneRegEx)
+    ? console.log("tel ok")
+    : console.log("Wypełnij poprawnie pole z nr telefonu");
+}
+
+function validateEmail(element) {
+  element.type === "email" && element.value.match(emailRegEx)
+    ? console.log("email ok")
+    : console.log("Wypełnij poprawnie pole z nr telefonu");
+}
+
+function validateAge(element) {
+  element.type === "number" &&
+  (element.valueAsNumber > 18 || element.valueAsNumber < 60)
+    ? console.log("wiek ok")
+    : console.log("Wypełnij poprawnie pole wiek");
+}
+
+function validateMarketingCheckbox(element) {
+  element.type === "checkbox" && element.checked === true
+    ? console.log("checkbox ok")
+    : console.log("Wypełnij poprawnie marketing");
+}
+
+function onSubmit(e) {
+  formInputs.forEach((element) => {
+    validateText(element);
+    validateNumber(element);
+    validateEmail(element);
+    validateAge(element);
+    validateMarketingCheckbox(element);
+  });
+  e.preventDefault();
+  console.log(e.target);
+}
+
+form.addEventListener("submit", onSubmit);
